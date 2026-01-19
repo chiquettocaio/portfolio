3113141a8fab64122fad724f722dac675da79997
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-01-19 15:41:39 -0300

    feat: adding contact area to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core'
 import { AboutComponent } from './sections/about/about.component'
+import { ContactComponent } from './sections/contact/contact.component'
 import { HeroComponent } from './sections/hero/hero.component'
 import { ProjectsComponent } from './sections/projects/projects.component'
 import { ResumeComponent } from './sections/resume/resume.component'
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AboutComponent, ResumeComponent, ProjectsComponent],
+  imports: [
+    HeroComponent,
+    AboutComponent,
+    ResumeComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
