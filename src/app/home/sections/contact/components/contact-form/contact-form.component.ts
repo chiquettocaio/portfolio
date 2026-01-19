c1611047f484d9f9801ee1fe33307c414dd6cf0a
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-01-19 15:39:46 -0300

    feat: contact-form creation and usage

---
+++ src/app/home/sections/contact/components/contact-form/contact-form.component.ts
@@ -0,0 +1,54 @@
+import { Component, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
+import { TranslatePipe } from '@ngx-translate/core'
+import { ButtonComponent } from '../../../../../shared/components/button/button.component'
+import { InputComponent } from '../../../../../shared/components/input/input.component'
+import { TextAreaComponent } from '../../../../../shared/components/textarea/textarea.component'
+
+@Component({
+  selector: 'app-contact-form',
+  imports: [
+    ReactiveFormsModule,
+    InputComponent,
+    TextAreaComponent,
+    ButtonComponent,
+    TranslatePipe
+  ],
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.scss'
+})
+export class ContactFormComponent {
+  private fb = inject(FormBuilder)
+
+  NAME_MIN_LENGTH: number = 2
+  NAME_MAX_LENGTH: number = 20
+  SUBJECT_MIN_LENGTH: number = 5
+  SUBJECT_MAX_LENGTH: number = 40
+  MESSAGE_MIN_LENGTH: number = 10
+
+  contactForm = this.fb.group({
+    firstName: ['', [
+      Validators.required,
+      Validators.minLength(this.NAME_MIN_LENGTH),
+      Validators.maxLength(this.NAME_MAX_LENGTH),
+      Validators.pattern('[a-zA-Z]*')
+    ]],
+
+    lastName: ['', [
+      Validators.required,
+      Validators.minLength(this.NAME_MIN_LENGTH),
+      Validators.maxLength(this.NAME_MAX_LENGTH),
+      Validators.pattern('[a-zA-Z]*')
+    ]],
+
+    subject: ['', [
+      Validators.required,
+      Validators.minLength(this.SUBJECT_MIN_LENGTH),
+      Validators.maxLength(this.SUBJECT_MAX_LENGTH)
+    ]],
+    message: ['', [
+      Validators.required,
+      Validators.minLength(this.MESSAGE_MIN_LENGTH)
+    ]]
+  }, { updateOn: 'blur' })
+}
--- src/app/home/sections/contact/contact.component.ts
+++ src/app/home/sections/contact/contact.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { TranslatePipe } from '@ngx-translate/core'
+import { ContactFormComponent } from './components/contact-form/contact-form.component'
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [TranslatePipe, ContactFormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-
 }
