1dd7738cb73d139c6ecd2685c09d145414ec20c9
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-01-20 00:22:34 -0300

    feat: nav menu component creation

---
+++ src/app/shared/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,47 @@
+import { AsyncPipe } from '@angular/common'
+import { Component, DOCUMENT, inject, signal } from '@angular/core'
+import { TranslatePipe } from '@ngx-translate/core'
+import { fromEvent, tap, throttleTime } from 'rxjs'
+import { TranslationConfigService } from '../../../services/translation/translation.service'
+import { IconComponent } from '../icon/icon.component'
+
+@Component({
+  selector: 'app-nav-menu',
+  imports: [IconComponent, TranslatePipe, AsyncPipe],
+  templateUrl: './nav-menu.component.html',
+  styleUrl: './nav-menu.component.scss'
+})
+export class NavMenuComponent {
+  private document = inject(DOCUMENT)
+  private translationConfigService = inject(TranslationConfigService)
+
+  TOP_DISTANCE_TO_MOVE_DOWN: number = 200
+
+  activeLink = signal<string>('hero')
+  navAtBottom = signal<boolean>(false)
+
+  scroller$ = fromEvent(window, 'scroll').pipe(
+    throttleTime(50),
+    tap(() => this.handleScroll())
+  )
+
+  currentLanguage = this.translationConfigService.currentLanguage
+
+  activateLink (link: string): void {
+    this.activeLink.set(link)
+  }
+
+  switchLanguage (): void {
+    const nextLang = this.currentLanguage() === 'pt-br'
+      ? 'en-us'
+      : 'pt-br'
+
+    this.translationConfigService.setLanguage(nextLang)
+  }
+
+  private handleScroll (): void {
+    const scrollTop = this.document.scrollingElement?.scrollTop ?? 0
+
+    this.navAtBottom.set(scrollTop >= this.TOP_DISTANCE_TO_MOVE_DOWN)
+  }
+}
