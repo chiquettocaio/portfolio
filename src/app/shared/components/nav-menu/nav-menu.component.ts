4bcf987bade71182b33db0c7f2810778124fe11a
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-01-20 10:05:45 -0300

    feat: monitoring elements to update nav bar on scroll

--- src/app/shared/components/nav-menu/nav-menu.component.ts
+++ src/app/shared/components/nav-menu/nav-menu.component.ts
@@ -1,37 +1,35 @@
-import { AsyncPipe } from '@angular/common'
-import { Component, DOCUMENT, inject, signal } from '@angular/core'
+import { Component, DOCUMENT, inject, OnInit, signal } from '@angular/core'
 import { TranslatePipe } from '@ngx-translate/core'
-import { fromEvent, tap, throttleTime } from 'rxjs'
 import { TranslationConfigService } from '../../../services/translation-config/translation-config.service'
+import { KeyString } from '../../models/generic-types.model'
 import { IconComponent } from '../icon/icon.component'
 
 @Component({
   selector: 'app-nav-menu',
-  imports: [IconComponent, TranslatePipe, AsyncPipe],
+  imports: [IconComponent, TranslatePipe],
   templateUrl: './nav-menu.component.html',
   styleUrl: './nav-menu.component.scss'
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   private document = inject(DOCUMENT)
   private translationConfigService = inject(TranslationConfigService)
 
   TOP_DISTANCE_TO_MOVE_DOWN: number = 200
+  IS_MOBILE: boolean = this.document.documentElement.clientWidth >= 768
 
   activeLink = signal<string>('hero')
   navBottom = signal<boolean>(false)
-
-  scroller$ = fromEvent(window, 'scroll').pipe(
-    throttleTime(100, undefined, {
-      leading: true,
-      trailing: true
-    }),
-    tap(() => this.handleScroll())
-  )
+  isImperativelyNavigating = signal<boolean>(false)
 
   currentLanguage = this.translationConfigService.currentLanguage
 
-  activateLink (link: string): void {
+  ngOnInit (): void {
+    this.initIntersectionObserver()
+  }
+
+  activateLink (link: string, viaIntersectionObserver: boolean = false): void {
     this.activeLink.set(link)
+    this.isImperativelyNavigating.set(!viaIntersectionObserver)
   }
 
   switchLanguage (): void {
@@ -42,9 +40,42 @@ export class NavMenuComponent {
     this.translationConfigService.setLanguage(nextLang)
   }
 
-  private handleScroll (): void {
-    const scrollTop = this.document.documentElement?.scrollTop ?? 0
+  private initIntersectionObserver (): void {
+    const heroSection = this.document.querySelector('#hero-section')
+    const aboutSection = this.document.querySelector('#about-section')
+    const resumeSection = this.document.querySelector('#resume-section')
+    const projectsSection = this.document.querySelector('#projects-section')
+    const contactSection = this.document.querySelector('#contact-section')
 
-    this.navBottom.set(scrollTop > this.TOP_DISTANCE_TO_MOVE_DOWN)
+    const mapping: KeyString<string> = {
+      'hero-section': 'hero',
+      'about-section': 'about',
+      'resume-section': 'resume',
+      'projects-section': 'projects',
+      'contact-section': 'contact'
+    }
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        const targetId: string = entry.target.id
+        const link: string = mapping[targetId]
+
+        if (entry.isIntersecting) {
+          this.navBottom.set(link !== 'hero')
+
+          if (!this.isImperativelyNavigating()) {
+            this.activateLink(link)
+          }
+
+          this.isImperativelyNavigating.set(link !== this.activeLink())
+        }
+      })
+    }, { rootMargin: '-40%' })
+
+    if (heroSection) observer.observe(heroSection)
+    if (aboutSection) observer.observe(aboutSection)
+    if (resumeSection) observer.observe(resumeSection)
+    if (projectsSection) observer.observe(projectsSection)
+    if (contactSection) observer.observe(contactSection)
   }
 }
