a7c12636799b0836626fee9d6c9230d807d45c44
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-01-23 13:18:31 -0300

    feat: social media links added to contact section; responsiveness

--- src/app/home/sections/contact/contact.component.ts
+++ src/app/home/sections/contact/contact.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core'
+import { AnchorComponent } from '@app/shared/components/anchor/anchor.component'
+import { IconComponent } from '@app/shared/components/icon/icon.component'
 import { TranslatePipe } from '@ngx-translate/core'
 import { ContactFormComponent } from './components/contact-form/contact-form.component'
 
 @Component({
   selector: 'app-contact',
-  imports: [TranslatePipe, ContactFormComponent],
+  imports: [
+    TranslatePipe,
+    ContactFormComponent,
+    AnchorComponent,
+    IconComponent
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
