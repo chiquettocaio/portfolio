7c52f4d7519930330a9ce319cd9f9193e62ae0a0
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-02-04 19:56:10 -0300

    chore: vercel analytics and speed-insights dependency install

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import { AppComponent } from './app/app.component'
 import { appConfig } from './app/app.config'
+
+inject()
+injectSpeedInsights()
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err))
