ca412687aa51e4d848e3967006bf8d5e7569e7c8
Caio Chiquetto <34581188+chiquettocaio@users.noreply.github.com>
2026-02-06 12:03:29 -0300

    feat: title update in the route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router'
 export const routes: Routes = [
   {
     path: '',
-    title: 'Caio Chiquetto - Front-end Engineer',
+    title: 'Caio Chiquetto - Front-end Engineer | Angular & SAP Spartacus',
     loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
   }
 ]
